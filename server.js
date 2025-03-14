f47568222903553902ba3fb6641058d592d80c76
Abrie PC <www.headoff.com@gmail.com>
2025-03-14 08:32:07 +0200

    add cors

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SECRET_VALUE = process.env.SECRET_VALUE || 'Default Secret';
+const ALLOWED_IP = process.env.ALLOWED_IP || '';
 
-// endpoint
+// middleware
+const ipFilter = (req, res, next) => {
+    const clientIp = req.ip || req.connection.remoteAddress;
+    if (clientIp === ALLOWED_IP) {
+        next();
+    } else {
+        res.status(403).send('Access denied');
+    }
+};
+
+// home
 app.get('/', (req, res) => {
     res.send('Welcome to the express server!');
 });
 
-// endpoint
-app.get('/w', (req, res) => {
+// text
+app.get('/text', (req, res) => {
     res.send('You are smashing this!');
+});
+
+// environment variable
+app.get('/environment', (req, res) => {
+    res.send(`Secret Value: ${SECRET_VALUE}`);
+});
+
+// secret message
+app.get('/secret', cors(), ipFilter, (req, res) => {
+    res.send('You have accessed a restricted endpoint!');
 });
 
 // start
